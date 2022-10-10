369d2e1929defba7e83961e07e1a0142a528e549
Jenil Patel <98316039+jenil-patel97@users.noreply.github.com>
2022-10-10 16:49:35 +0530

    fix: Footer & Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,14 @@ import React, { useState } from "react";
 import logo from "../assests/cloud-icon.png";
 
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+// eslint-disable-next-line no-unused-vars
+import { Link, animateScroll as scroll, } from 'react-scroll'
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
   const handleClick = () => setNav(!nav);
 
-//   const handleClose = () => setNav(!nav);
+  const handleClose = () => setNav(!nav);
 
   return (
     <div className="w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg">
@@ -19,12 +21,12 @@ const Navbar = () => {
             alt="logo"
             className="text-3xl font-bold mr-4 sm:text-4xl"
           ></img>
-          <ul className="hidden md:flex">
-            <li>Home</li>
-            <li>About</li>
-            <li>Support</li>
-            <li>Platforms</li>
-            <li>Pricing</li>
+          <ul className="hidden md:flex cursor-default">
+          <li><Link to="home" smooth={true} duration={500}>Home</Link></li>
+          <li><Link to="about" smooth={true} offset={-200} duration={500}>About</Link></li>
+          <li><Link to="support" smooth={true} offset={-50} duration={500}>Support</Link></li>
+          <li><Link to="pricing" smooth={true} offset={-100} duration={500}>Pricing</Link></li>
+          <li><Link to="footer" smooth={true} offset={-50} duration={500}>Contact</Link></li>
           </ul>
         </div>
         <div className="hidden md:flex pr-4">
@@ -39,11 +41,11 @@ const Navbar = () => {
       </div>
 
       <ul className={!nav ? "hidden" : "absolute bg-zinc-200 w-full px-8 text-center"}>
-        <li className="border-b-2 border-zinc-300 w-full">Home</li>
-        <li className="border-b-2 border-zinc-300 w-full">About</li>
-        <li className="border-b-2 border-zinc-300 w-full">Support</li>
-        <li className="border-b-2 border-zinc-300 w-full">Platforms</li>
-        <li className="border-b-2 border-zinc-300 w-full">Pricing</li>
+      <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose} to="home" smooth={true} duration={500}>Home</Link></li>
+          <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose} to="about" smooth={true} offset={-200} duration={500}>About</Link></li>
+          <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose} to="support" smooth={true} offset={-50} duration={500}>Support</Link></li>
+          <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose} to="pricing" smooth={true} offset={-100} duration={500}>Pricing</Link></li>
+          <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose} to="footer" smooth={true} offset={-50} duration={500}>Contact</Link></li>
 
         <div className="flex flex-col my-4">
           <button className="bg-transparent text-black px-8 py-3 mb-4">
