15fc9fdce257fdf3d068b3cbde21ebff8df21d11
Jenil Patel <98316039+jenil-patel97@users.noreply.github.com>
2022-10-10 11:04:17 +0530

    fix: Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   const [nav, setNav] = useState(false);
   const handleClick = () => setNav(!nav);
 
-  const handleClose = () => setNav(!nav);
+//   const handleClose = () => setNav(!nav);
 
   return (
     <div className="w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg">
