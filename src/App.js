a87e2c2c1ee979d64f95a02901c19dbd8e276c6f
Jenil Patel <98316039+jenil-patel97@users.noreply.github.com>
2022-10-10 15:43:25 +0530

    feat: Pricing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import About from "./components/About";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Pricing from "./components/Pricing";
 import Support from "./components/Support";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <About />
       <Support />
+      <Pricing />
     </>
   );
 }
